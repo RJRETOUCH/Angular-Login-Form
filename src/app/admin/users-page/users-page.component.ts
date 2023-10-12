65fe67cb634075f2db70c3a576fea9cadfad606e
Rajneesh <rjretouch@gmail.com>
2023-10-12 19:07:50 +0530

    initial commit

---
+++ src/app/admin/users-page/users-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+
+@Component({
+  selector: 'app-users-page',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.scss']
+})
+export class UsersPageComponent {
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  logout() {
+    this.authService.logout()
+  }
+
+}
